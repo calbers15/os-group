658d0fddef24174df2688ec5a5af71fc3ea3b265
calbers15 <cameron_albers@student.uml.edu>
2024-02-12 17:09:19 -0500

    added pids

--- p1/main.c
+++ p1/main.c
@@ -10,11 +10,15 @@ int main(){
     pid = fork();
 
     if(pid > 0){
+        printf("Waiting for child to finish\n");
         wait(NULL);
         printf("Parent Process\n");
+        printf("Parent ID: %d", (int) getpid());
     }
     else if(pid == 0){
         printf("Child Process\n");
+        printf("Child ID: %d", (int) getpid());
+
     }
 
     return 0;
