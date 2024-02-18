eb96034d1a76e8ab08843165ec621b50e8e4011b
calbers15 <cameron_albers@student.uml.edu>
2024-02-17 23:30:26 -0500

    added part 2

--- p1/main.c
+++ p1/main.c
@@ -6,22 +6,32 @@
 #include <sys/types.h>
 
 int main(){
-    pid_t pid;
-    pid = fork();
+    pid_t pid1, pid2;
+    pid1 = fork();
+    
+    if(pid1 == 0){
+            printf("Child 1 Process\n");
+            printf("Child 1 ID: %d\n\n", (int) getpid());
+            printf("Child 1 Finished\n\n");
 
-    if(pid > 0){
+        }
+
+    else{
+        wait(NULL);
+        printf("First child complete\n");
+        printf("Parent creating second child\n");
+        pid2 = fork();
+        if(pid2 == 0){
+            printf("Child 2 Process\n");
+            printf("Child 2 ID: %d\n\n", (int) getpid());
+            printf("Child 2 Finished\n\n");
+        }
         printf("Waiting for child to finish\n");
         wait(NULL);
-        printf("Child Completed\n\n");
         printf("Parent Process\n");
-        printf("Parent ID: %d\n\n", (int) getpid());
+        printf("Parent ID: %d\n", (int) getpid());
     }
-    else if(pid == 0){
-        printf("Child Process\n");
-        printf("Child ID: %d\n\n", (int) getpid());
-        printf("Child Finished\n\n");
-
-    }
+    
 
     return 0;
 }
