9367623a21dc1677d68d25d832d55531712e3cbf
calbers15 <cameron_albers@student.uml.edu>
2024-02-17 23:47:08 -0500

    two child processes

--- p1/main.c
+++ p1/main.c
@@ -7,31 +7,28 @@
 
 int main(){
     pid_t pid1, pid2;
+    printf("Parent Process\n");
+    printf("Parent ID: %d\n\n", (int) getpid());
     pid1 = fork();
     
-    if(pid1 == 0){
-            printf("Child 1 Process\n");
-            printf("Child 1 ID: %d\n\n", (int) getpid());
-            printf("Child 1 Finished\n\n");
-
-        }
+    if (pid1 == 0){
+        printf("Child 1 Process\n");
+        printf("Child 1 ID: %d\n", (int) getpid());
+        printf("Child 1 Complete\n\n");
+    }
 
     else{
-        wait(NULL);
-        printf("First child complete\n");
-        printf("Parent creating second child\n");
+        printf("Parent creating Child Process 2\n");
         pid2 = fork();
         if(pid2 == 0){
             printf("Child 2 Process\n");
-            printf("Child 2 ID: %d\n\n", (int) getpid());
-            printf("Child 2 Finished\n\n");
+            printf("Child 2 ID: %d\n", (int) getpid());
         }
-        printf("Waiting for child to finish\n");
-        wait(NULL);
-        printf("Parent Process\n");
-        printf("Parent ID: %d\n", (int) getpid());
-    }
-    
+        else{
+            wait(NULL);
+            printf("Child 2 Complete\n\n");
+        }
 
+    }
     return 0;
 }
