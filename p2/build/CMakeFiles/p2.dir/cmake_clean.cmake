b773c8fe44ab484ff1352cb814f1528e616e78bb
calbers15 <cameron_albers@student.uml.edu>
2024-03-23 22:26:37 -0400

    grade me

--- p2/build/CMakeFiles/p2.dir/cmake_clean.cmake
+++ p2/build/CMakeFiles/p2.dir/cmake_clean.cmake
@@ -1,11 +1,13 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/p2.dir/main.cpp.o"
-  "CMakeFiles/p2.dir/main.cpp.o.d"
+  "CMakeFiles/p2.dir/main.c.o"
+  "CMakeFiles/p2.dir/main.c.o.d"
+  "CMakeFiles/p2.dir/prod_cons_MT.c.o"
+  "CMakeFiles/p2.dir/prod_cons_MT.c.o.d"
   "p2"
   "p2.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang C)
   include(CMakeFiles/p2.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
