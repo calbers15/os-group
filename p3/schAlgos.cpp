121b6070a47e4c39343de356a0de880f21281efa
calbers15 <cameron_albers@student.uml.edu>
2024-04-25 16:58:54 -0400

    added function declarations for findPriority and findShortJob

--- p3/schAlgos.cpp
+++ p3/schAlgos.cpp
@@ -7,3 +7,34 @@
 
 using namespace std;
 
+int findShortJob(vector<Process> processes, int currentTime){
+    int min_burst = -1;
+    int min_index = -1;
+
+    for (int i = 0; i < processes.size()-1; i++){
+        if (processes[i].arrivalTime <= currentTime && processes[i].burstTime > 0){
+            if (min_burst == -1 || processes[i].burstTime < min_burst){
+                min_burst = processes[i].burstTime;
+                min_index = i;
+            }
+        }
+    }
+    return min_index;
+}
+
+int findPriority(vector<Process> processes, int currentTime){
+    int min_burst = -1;
+    int min_index = -1;
+
+    for(int i = 0; i <processes.size()-1; i++){
+        if (processes[i].arrivalTime <= currentTime && processes[i].burstTime > 0){
+            if(min_burst == -1 || processes[i].procPri < min_burst){
+                min_burst = processes[i].procPri;
+                min_index = i;
+            }
+        }
+    }
+
+    return min_index;
+}
+
--- p3/schAlgos.h
+++ p3/schAlgos.h
@@ -10,6 +10,65 @@
 
 using namespace std;
 
+//define 
+class Process{
+public:
+    float pid;
+    float burstTime;
+    float procPri;
+    float arrivalTime;
+    float startTime = -1;
+    float waitTime;
+    float turnaroundTime;
+    bool hasRun = false;
+    bool isComplete = false;
+};
 
+//define algorithmMetrics class to keep track of the metrics for each scheudling algorithm
+class algorithmMetrics{
+public:
+    float contextSwitches = 0;
+    float avTurnaround = 0;
+    float avWait = 0;
+};
+
+//function to sort through data and find shortest job
+int findShortJob(vector<Process> processes, int currentTime);
+
+//function to sort through data and find priority
+int findPriority(vector<Process> processes, int currentTime);
+
+//print first come first serve function
+void printFCFS(vector<Process> proc, string s2, int currentTime, int currentProcess);
+
+//print shortest job first function
+void printSJF(vector<Process> proc, string s2, int currentTime, int currentProcess);
+
+//print non preemptive prioriry
+void printNPP(vector<Process> proc, string s2, int currentTime, int currentProcess);
+
+//priority scheduling function
+algorithmMetrics priority(vector<Process> processes, string s1, int numProccess, int interv);
+
+//shortest job first function
+algorithmMetrics sjf(vector<Process> processes, string s1, int numProcess, int interv);
+
+//shortest time to completion first function
+algorithmMetrics stcf(vector<Process> processes, string s1, int numProcess, int interv);
+
+//round robin function
+algorithmMetrics rr(vector<Process> processes, string s1, int numProcess, int interv);
+
+//first come first serve function
+algorithmMetrics fcfs(vector<Process> processes, string s1, int numProcess, int interv);
+
+//waiting time comparison
+bool WTComp(algorithmMetrics x, algorithmMetrics y);
+
+//turnaround time comparison
+bool TTComp(algorithmMetrics x, algorithmMetrics y);
+
+//completion time comparison
+bool CSComp(algorithmMetrics x, algorithmMetrics y);
 
 #endif
