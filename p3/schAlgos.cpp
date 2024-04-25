208e76abaa7f0001427a37fbe9f14b7ef3db1c27
calbers15 <cameron_albers@student.uml.edu>
2024-04-25 17:54:28 -0400

    wrote printNPP function

--- p3/schAlgos.cpp
+++ p3/schAlgos.cpp
@@ -8,13 +8,13 @@
 using namespace std;
 
 int findShortJob(vector<Process> processes, int currentTime){
-    int min_burst = -1;
+    int min_burstTime = -1;
     int min_index = -1;
 
     for (int i = 0; i < processes.size()-1; i++){
         if (processes[i].arrivalTime <= currentTime && processes[i].burstTime > 0){
-            if (min_burst == -1 || processes[i].burstTime < min_burst){
-                min_burst = processes[i].burstTime;
+            if (min_burstTime == -1 || processes[i].burstTime < min_burstTime){
+                min_burstTime = processes[i].burstTime;
                 min_index = i;
             }
         }
@@ -22,14 +22,14 @@ int findShortJob(vector<Process> processes, int currentTime){
     return min_index;
 }
 
-int findPriority(vector<Process> processes, int currentTime){
-    int min_burst = -1;
+int findprocPri(vector<Process> processes, int currentTime){
+    int min_burstTime = -1;
     int min_index = -1;
 
     for(int i = 0; i <processes.size()-1; i++){
         if (processes[i].arrivalTime <= currentTime && processes[i].burstTime > 0){
-            if(min_burst == -1 || processes[i].procPri < min_burst){
-                min_burst = processes[i].procPri;
+            if(min_burstTime == -1 || processes[i].procPri < min_burstTime){
+                min_burstTime = processes[i].procPri;
                 min_index = i;
             }
         }
@@ -53,11 +53,11 @@ void printSJF(vector<Process> proc, string s2, int currentTime, int currentProce
     out << "t = " << currentTime << endl;
 
     if (proc[currentProcess].arrivalTime == currentTime){
-        out << "CPU: Loading process " << proc[currentProcess].pid << " (CPU Burst = " << proc[currentProcess].burstTime << ")" << endl;
+        out << "CPU: Loading process " << proc[currentProcess].pid << " (CPU burstTime = " << proc[currentProcess].burstTime << ")" << endl;
     }
 
     else if (proc[currentProcess].startTime <= currentTime && proc[currentProcess].burstTime > 0){
-        out << "CPU: Running process " << proc[currentProcess].pid << " (remaining CPU Burst = " << proc[currentProcess].burstTime << ")" << endl;
+        out << "CPU: Running process " << proc[currentProcess].pid << " (remaining CPU burstTime = " << proc[currentProcess].burstTime << ")" << endl;
     }
 
     else if (proc[currentProcess].burstTime == 0 && findShortJob(proc, currentTime) == -1) {
@@ -65,7 +65,7 @@ void printSJF(vector<Process> proc, string s2, int currentTime, int currentProce
     } 
 
     else if (proc[currentProcess].burstTime == 0) {
-        out << "CPU: Finishing process " << proc[currentProcess].pid << "; loading process " << proc[findShortJob(proc, currentTime)].pid << " (CPU burst = " << proc[findShortJob(proc, currentTime)].burstTime << ")" << endl;
+        out << "CPU: Finishing process " << proc[currentProcess].pid << "; loading process " << proc[findShortJob(proc, currentTime)].pid << " (CPU burstTime = " << proc[findShortJob(proc, currentTime)].burstTime << ")" << endl;
     }
 
     out << "Ready queue: ";
@@ -110,4 +110,115 @@ void printSJF(vector<Process> proc, string s2, int currentTime, int currentProce
 
     out << endl;
     out.close();
+}
+
+void printNPP(vector<Process> proc, string s2, int currentTime, int currentProcess){
+    ofstream out;
+
+    out.open(s2, ios_base::app);
+
+    vector<Process> readyProc;
+    int y = 0;
+
+    int empty = 0;
+
+    out << endl;
+
+    out << "t = " << currentTime << endl;
+
+    if(proc[currentProcess].arrivalTime == currentTime){
+        out << "CPU: Loading process " << proc[currentProcess].pid << " (CPU burstTime = " << proc[currentProcess].burstTime << ")" << endl;
+    }
+
+    else if(proc[currentProcess].startTime <= currentTime && proc[currentProcess].burstTime > 0){
+        out << "CPU: Running process " << proc[currentProcess].pid << " (remaining CPU burstTime = " << proc[currentProcess].burstTime << ")" << endl;
+    }
+
+    else if(proc[currentProcess].burstTime == 0 && findprocPri(proc, currentTime) == -1){
+        out << "CPU: Finishing process " << proc[currentProcess].pid << endl;
+    }
+
+    else if(proc[currentProcess].burstTime == 0){
+        out << "CPU: Finishing process " << proc[currentProcess].pid << "; loading process " << proc[findprocPri(proc, currentTime)].pid << " (CPU burstTime = " << proc[findprocPri(proc, currentTime)].burstTime << ")" << endl;
+    }
+
+    out << "ready queue: ";
+
+    int finished = 0;
+
+    for (int x = 0; x < proc.size() - 1; x++) {
+
+        if (proc[x].arrivalTime <= currentTime && proc[x].burstTime > 0 && proc[x].startTime == -1) {
+
+            if(proc[findprocPri(proc, currentTime - 1)].burstTime > 0 && findprocPri(proc, currentTime - 1) != currentProcess && proc[findprocPri(proc, currentTime - 1)].burstTime && finished == 0){
+
+                readyProc.push_back(proc[findprocPri(proc, currentTime - 1)]);
+                finished = 1;
+
+            }
+
+            if(proc[x].pid != proc[findprocPri(proc, currentTime - 1)].pid){
+
+                readyProc.push_back(proc[x]);
+
+            }
+
+        }
+
+    }
+
+    for (int w = 0; w < readyProc.size(); w++){
+ 
+        for (int j = 0; j < readyProc.size() - 1; j++){
+
+            if (readyProc[j].procPri > readyProc[j + 1].procPri){
+
+                swap(readyProc[j], readyProc[j + 1]);
+
+            }
+
+        }
+    }
+    if (currentTime == 0) {
+        for (int i = 1; i < readyProc.size(); i++) {
+
+            out << readyProc[i].pid;
+
+            empty = 1; // Set the empty flag to indicate that the ready queue is not empty
+
+            // Add a separator between process pids in the ready queue
+            if (i < readyProc.size() - 1) {
+
+                out << "-";
+
+            }
+
+        }
+    }
+    else if (currentTime > 0) {
+        for (int i = 0; i < readyProc.size(); i++) {
+
+            out << readyProc[i].pid;
+
+            empty = 1; // Set the empty flag to indicate that the ready queue is not empty
+
+            // Add a separator between process pids in the ready queue
+            if (i < readyProc.size() - 1) {
+
+                out << "-";
+
+            }
+
+        }
+    }
+    // If the empty flag is not set, the ready queue is empty
+    if (empty == 0) {
+
+        out << "empty";
+
+    }
+
+    out << endl;
+    out.close(); // Close the output file
+
 }
