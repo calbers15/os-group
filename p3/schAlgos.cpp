ff3e9cc593f3ade750f797ab6a834283312ce71a
calbers15 <cameron_albers@student.uml.edu>
2024-04-27 19:33:23 -0400

    wrote sjf function

--- p3/schAlgos.cpp
+++ p3/schAlgos.cpp
@@ -236,60 +236,157 @@ void printFCFS(vector<Process> proc, string s2, int currentTime, int curProc){
     out << "t = " << currentTime << endl;
 
     if (proc[curProc].arrivalTime == currentTime) {
-
         out << "CPU: Loading process " << proc[curProc].pid << " (CPU burst = " << proc[curProc].burstTime << ")" << endl;
-
     }
     
     else if (proc[curProc].startTime <= currentTime && proc[curProc].burstTime > 0) {
-
         out << "CPU: Running process " << proc[curProc].pid << " (remaining CPU burst = " << proc[curProc].burstTime << ")" << endl;
-
     } 
     
     else if (proc[curProc].burstTime == 0 && curProc + 1 > proc.size() - 2) {
-    
         out << "CPU: Finishing process " << proc[curProc].pid << endl;
-
     } 
     
-    else if (proc[curProc].burstTime == 0) {
-    
+    else if (proc[curProc].burstTime == 0) {   
         out << "CPU: Finishing process " << proc[curProc].pid << "; loading process " << proc[curProc + 1].pid << " (CPU burst = " << proc[curProc + 1].burstTime << ")" << endl;
-
     }
-
     
     out << "Ready queue: ";
 
     for (int i = 0; i < proc.size() - 1; i++) {
-
-        
         if(proc[i].arrivalTime <= currentTime && proc[i].burstTime > 0 && proc[i].startTime == -1){
-
             out << proc[i].pid;
-
             empty = 1; 
-
-            
             if (i < proc.size() - 2 && proc[i + 1].arrivalTime <= currentTime) {
-
                 out << "-";
-
             }
-
         }
-
     }
 
-    
     if (empty == 0) {
-
         out << "empty";
-
     }
 
     out << endl;
     out.close();
 
+}
+
+algorithmMetrics sjf(vector<Process> processes, string s1, int numProcess, int interv){
+    ofstream out;
+
+    out.open(s1, ios_base::app);
+
+    algorithmMetrics metric;
+    int currentTime = 0;
+    int finishedProc = 0;
+    int currentProc = findShortJob(processes, currentTime);
+    int tempId;
+    string procSeq = "";
+    int contextSw = 0;
+    float numProcessFloat = numProcess;
+
+    out << endl;
+
+    out << "*** SJF SCHEDULING ***" << endl;
+
+    printSJF(processes, s1, currentTime, currentProc);
+
+    while(finishedProc < numProcess){
+
+        // Find the index of the process with the shortest job
+        currentProc = findShortJob(processes, currentTime);
+	    
+	//Set start time
+	    if(processes[currentProc].startTime = -1){
+	
+		    processes[currentProc].startTime = currentTime;
+	
+	    }
+	    
+        // If a process is found, execute it
+        if (currentProc != -1) {
+            // calculate the wait time for the process
+            processes[currentProc].waitTime = currentTime - processes[currentProc].arrivalTime;
+            processes[currentProc].turnaroundTime = processes[currentProc].waitTime + processes[currentProc].burstTime;
+
+            // Iterate through the process burst time and update the current time
+            while (processes[currentProc].burstTime > 0) {
+
+		        processes[currentProc].burstTime--;
+                currentTime++;
+
+                // Print scheduler state if the current time is a multiple of the interval
+                if (currentTime % interv == 0 || currentTime % interv == interv){
+
+                    printSJF(processes, s1, currentTime, currentProc);
+
+                }
+                
+            }
+
+            // Increment the number of finished processes
+            finishedProc++;
+            tempId = processes[currentProc].pid;
+            procSeq += to_string(tempId) + "-";
+            contextSw++;
+
+        } 
+        
+        else {
+
+            // If no process was found, increment the current time
+            currentTime++;
+
+            // Print scheduler state if the current time is a multiple of the interval
+            if (currentTime % interv == 0 ){
+
+                printSJF(processes, s1, currentTime, currentProc);
+
+            }
+        }
+    }
+    procSeq.pop_back(); // Remove the last hyphen
+    metric.contextSwitches = contextSw;
+    out << endl;
+    out << "*********************************************************" << endl;
+    out << "SJF Summary (WT = wait time, TT = turnaround time):" << endl;
+    out << "PID     WT     TT" << endl;
+
+    for(int i = 0; i < numProcess; i++){
+
+        if(processes[i].waitTime < 10){
+
+            out << " " << processes[i].pid << "      " << processes[i].waitTime << "      " << processes[i].turnaroundTime << endl;
+        
+        }
+
+        if(processes[i].waitTime >= 10 && processes[i].waitTime < 100){
+
+            out << " " << processes[i].pid << "      " << processes[i].waitTime << "     " << processes[i].turnaroundTime << endl;
+        
+        }
+        if (processes[i].waitTime >= 100){
+
+            out << " " << processes[i].pid << "     " << processes[i].waitTime << "     " << processes[i].turnaroundTime << endl;
+        
+        }
+
+    }
+
+    for(int x = 0; x < numProcess; x++){
+
+        metric.avWait = metric.avWait + processes[x].waitTime;
+        metric.avTurnaround = metric.avTurnaround + processes[x].turnaroundTime;
+
+    }
+    metric.avWait = metric.avWait / numProcessFloat;
+    metric.avTurnaround = metric.avTurnaround / numProcessFloat;
+
+    out << "AVG" << "     " << metric.avWait << "   " << metric.avTurnaround << endl;
+
+    out << "Process sequence: " << procSeq << endl;
+    out << "Context Switches: " << contextSw << endl;
+    out.close(); // Close the output file
+    return metric;
 }
