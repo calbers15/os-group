c3f1a3c9e445ce89e2dc2af3068e374da29c428f
calbers15 <cameron_albers@student.uml.edu>
2024-04-29 16:32:21 -0400

    wrote header file, defined classses, member functions

---
+++ p4/tFunctions.h
@@ -0,0 +1,41 @@
+#ifndef TFUNCTIONS_H
+#define TFUNCTIONS_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <thread>
+#include <vector>
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <semaphore.h>
+
+using namespace std;
+
+class PageTable{
+    public:
+        int valid = 0;
+        int physicalAddress = 0;
+        int pageNumber = 0;
+        int frameNumber = 0;
+};
+
+class FrameData{
+    public:
+        int frameNumber = 0;
+        int physicalAddress = 0;
+};
+
+string toHexadecimal(int num);
+
+void evict(int threadNumber, string outputFile, int pageSize);
+
+void processThread(int pageSize, int threadNumber, string threadFile, string outFile);
+
+void processThread2(int pageSize, int threadNumber, string threadFile, string outFile);
+
+void processThread3(int pageSize, int threadNumber, string threadFile, string outFile);
+
+void processThread4(int pageSize, int threadNumber, string threadFile, string outFile);
+
+#endif
